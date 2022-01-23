2a318e2ed56b04e5c55bfe4d1944e31f6768178f
Sarahí Fernández <sarahifernandeza@gmail.com>
2022-01-22 18:39:37 -0600

    histograms

--- code/etl.py
+++ code/etl.py
@@ -10,7 +10,7 @@ def read_local_path(file_path_ch_cust):
     #Transform
     df = df.drop(columns=['Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_1', 'Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_2'])
     #Load
-    df.to_csv(r"c:\Users\sarah\Desktop\ch_customers\data\bank_churners_drop_incorrect_columns.csv")
+    df.to_csv(r"c:\Users\sarah\Desktop\ch_customers\data\bank_churners_drop_incorrect_columns.csv", index=False)
     return df
 
 
